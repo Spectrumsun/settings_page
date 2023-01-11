d0a4bb59865a27ddb7d73dedadfc6d895316d67d
Taiwo Sunday <taiwo.sunday@wellth.com>
2023-01-11 13:48:17 +0100

    update with files

--- src/Setting/index.js
+++ src/Setting/index.js
@@ -35,19 +35,19 @@ const Setting = () => {
         <li>Integrations</li>
         <li className="rounded-br-lg rounded-tr-lg">API</li>
       </ul>
-      <h3 className="mt-10 text-xl pl-5 md:pl-0">Payment method</h3>
+      <h3 className="mt-10 text-xl pl-5 md:pl-0 text-gray-900">Payment method</h3>
       <p className="font-light text-xs mb-5 text-gray-500 pl-5 md:pl-0">Update your billing and address.</p>
       <hr className=" mb-10"/>
       <div className="flex flex-wrap pl-5 md:pl-0">
         <div className="mr-20">
-          <h4>Contact email</h4>
+          <h4 className="text-gray-700">Contact email</h4>
           <p className="font-light text-xs mb-5 text-gray-500">Where should invoices be sent?</p>
         </div>
         <div>
           <div className="flex">
             <input type="radio" id="email" name="default_email" value="olivia@untitledui.com" />
             <span className="ml-2">
-              <label for="html">Send to my account email</label><br />
+              <label for="html text-gray-700">Send to my account email</label><br />
               <p className="font-light text-xs mb-5 text-gray-500">olivia@untitledui.com</p>
             </span>
           </div>
@@ -55,7 +55,7 @@ const Setting = () => {
           <div className="flex">
             <input type="radio" id="email" name="alternative_email" checked="checked"  value="biling@untitledui.com" />
             <span className="ml-2">
-                <label for="html">Send to an alternative email</label><br />
+                <label for="html" className="text-gray-700">Send to an alternative email</label><br />
               <div className="setting__email mt-3">
                 <Email />
                 <p className="font-light text-xs ml-3">biling@untitledui.com</p>
@@ -71,7 +71,7 @@ const Setting = () => {
 
       <div className="setting__pay flex w-full pl-5 md:pl-0 pr-5 md:pr-0">
         <div className="mr-20">
-          <h4>Card details</h4>
+          <h4 className="text-gray-700">Card details</h4>
           <p className="font-light text-xs mb-5 text-gray-500">Select default payment method.</p>
         </div>
         <div  className="flex w-full flex-col">
@@ -103,10 +103,10 @@ const Setting = () => {
       </div>
       <div className="mt-10 mb-5 pl-5 md:pl-0 pr-5 md:pr-0">
         <div className="flex justify-between flex-col md:flex-row">
-          <p>Billing History</p>
+          <p className="text-gray-900">Billing History</p>
           <button className="flex p-3 setting__email justify-between items-center !w-max mt-5 md:mt-0">
             <Button />
-            <span className="ml-3">Download all</span>
+            <span className="ml-3 text-gray-700">Download all</span>
           </button>
         </div>
       </div>
